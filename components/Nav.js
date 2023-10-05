e1c18c96686a4a66a08354f1424bdc3e2f698e64
HoangNam2k3 <namhoang6266@gmail.com>
2023-10-05 21:24:09 +0700

    Update

--- components/Nav.js
+++ components/Nav.js
@@ -1,14 +1,26 @@
-import { View, Text, Image } from 'react-native'
+import { useNavigation } from '@react-navigation/native';
+import { View, Text, Image, TouchableOpacity } from 'react-native';
 
-const Nav = ({tit}) => {
-  return (
-    <View style={{flexDirection: 'row', alignItems:'center', justifyContent: 'space-between', paddingHorizontal: 18}}>
-      <Image  source={require('../assets/Iicon_Back.png')}/>
-      <View style={{flex: 1, alignItems: 'center'}}>
-        <Text style={{fontSize: 22, fontWeight: 700}}>{tit}</Text>
-      </View>
-    </View>
-  )
-}
+const Nav = ({ tit }) => {
+    const navigation = useNavigation();
 
-export default Nav
+    return (
+        <View
+            style={{
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'space-between',
+                paddingHorizontal: 18,
+            }}
+        >
+            <TouchableOpacity onPress={() => navigation.goBack()}>
+                <Image source={require('../assets/Iicon_Back.png')} />
+            </TouchableOpacity>
+            <View style={{ flex: 1, alignItems: 'center' }}>
+                <Text style={{ fontSize: 22, fontWeight: 700 }}>{tit}</Text>
+            </View>
+        </View>
+    );
+};
+
+export default Nav;
--- screens/OnMap.js
+++ screens/OnMap.js
@@ -1,15 +1,15 @@
-import { View, Text, Image } from 'react-native'
-import Nav from '../components/Nav'
+import { View, Text, Image } from 'react-native';
+import Nav from '../components/Nav';
 
 const OnMap = () => {
-  return (
-    <View style={{marginVertical: 44}}>
-      <Nav tit={'Next to you'}/>
-      <View style={{width: '100%', padding: 18}}>
-        <Image style={{width: '100%', height: '100%'}} source={require('../assets/Maps.png')}/>
-      </View>
-    </View>
-  )
-}
+    return (
+        <View style={{ marginVertical: 44 }}>
+            <Nav tit={'Next to you'} />
+            <View style={{ width: '100%', padding: 18 }}>
+                <Image style={{ width: '100%', height: '100%' }} source={require('../assets/Maps.png')} />
+            </View>
+        </View>
+    );
+};
 
-export default OnMap
+export default OnMap;
